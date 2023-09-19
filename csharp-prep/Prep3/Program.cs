958353b62f873a9d0118950d306f40d9393bade2
SnowMoto <motogirlmonday@gmail.com>
2023-09-19 13:15:24 -0600

    update prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,100);
+
+        int guess = -1;
+        int timesguessed = 0;
+
+        while (guess != magic)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+        if (guess < magic)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magic)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+            timesguessed++;
+        }
+        }
     }
 }
