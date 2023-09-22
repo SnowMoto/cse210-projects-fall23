71824508649c9a277dad24a33c0dde18bb2ef5c5
SnowMoto <motogirlmonday@gmail.com>
2023-09-21 18:20:14 -0600

    update prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program");
+        Console.Write("Please enter your name: ");
+        string nameinput = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        string favnumber = Console.ReadLine();
+        int number = int.Parse(favnumber);
+
+        int square = number * number;
+
+        Console.WriteLine($"{nameinput}, the square of your number is {square} ");
     }
 }
