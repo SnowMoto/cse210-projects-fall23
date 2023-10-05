572288977c52b84c894234ad82d2c79ede68ff64
SnowMoto <motogirlmonday@gmail.com>
2023-10-05 16:22:47 -0600

    update

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -11,14 +11,14 @@ class Program
 
         while (number !=5)
         {
-            Console.WriteLine("Journal Menu: ");
+            Console.WriteLine("Welcome to your Journal Menu: ");
             Console.WriteLine();
             Console.WriteLine("1. New Entry");
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Save");
             Console.WriteLine("4. Load");
             Console.WriteLine("5. Quit");
-            Console.Write("Menu Option: ");
+            Console.Write("Select Menu Option: ");
             string userinput = Console.ReadLine();
             number = int.Parse(userinput);   
 
@@ -26,18 +26,18 @@ class Program
             {
                 Entry entry = new Entry();
 
+                 // Get a Prompt. (Hint: Create Prompt first.)
+                Prompts prompt = new Prompts();
+                string newPrompt = prompt.ToGetRandomGenerator();
+                Console.WriteLine($"{newPrompt}");
+
                 // Get entry text from user.
                 System.Console.WriteLine("");
                 string entryText = Console.ReadLine();
 
-                // Place it to the entry text variable.
                 entry._entryText = entryText;
 
-                // Get a Prompt. (Hint: Create Prompt first.)
-                Prompts prompt = new Prompts();
-                prompt.ToGetRandomGenerator();
-                
-                // Get current date.(Hint: Look at the instructions.)
+                // Get date.
                 DateTime theCurrentTime = DateTime.Now;
                 string dateText = theCurrentTime.ToShortDateString();
                 Console.WriteLine($"{dateText}");
